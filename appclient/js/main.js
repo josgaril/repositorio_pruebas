f0c09f17bfbffa40f585659f9c7ff4b46a953f7b
josgaril <josgaril@hotmail.com>
2020-04-01 13:31:30 +0200

    Agregado main.js con los distintos tipos de console

---
+++ appclient/js/main.js
@@ -0,0 +1,5 @@
+console.info('console.info es para mostrar mensajes de información depurando en el navegador');
+console.debug('console.debug es una traza de tipo DEBUG');
+console.trace('console.trace suele ser para tacear o decir <annonymous>');
+console.warn('console.warn se utiliza para mandar mensajes de warning');
+console.error('console.error se utiliza para mandar mensajes de error. Ej: Ha habido un error en la aplicación');
