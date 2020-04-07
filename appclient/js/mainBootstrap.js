e0fa291a0db67555f801a158e6ae0b22344d22a1
josgaril <josgaril@hotmail.com>
2020-04-07 12:30:28 +0200

    IndexBootStrap con selector de sexo funcionando

--- appclient/js/mainBootstrap.js
+++ appclient/js/mainBootstrap.js
@@ -1,58 +1,71 @@
+let personas = [{
+    "nombre": "Occonnor",
+    "sexo": "M",
+    "avatar": "img/avatar1.png"
+  },
+  {
+    "nombre": "Pepa",
+    "sexo": "M",
+    "avatar": "img/avatar2.png"
+  },
+  {
+    "nombre": "JoseMari",
+    "sexo": "H",
+    "avatar": "img/avatar3.png"
+  },
+  {
+    "nombre": "JuanCarlos",
+    "sexo": "H",
+    "avatar": "img/avatar4.png"
+  },
+  {
+    "nombre": "Monica",
+    "sexo": "M",
+    "avatar": "img/avatar5.png"
+  },
+  {
+    "nombre": "Alberto",
+    "sexo": "H",
+    "avatar": "img/avatar6.png"
+  },
+  {
+    "nombre": "Rafael",
+    "sexo": "H",
+    "avatar": "img/avatar7.png"
+  }
+]
+
 window.addEventListener('load', init());
 
 function init() {
 
   console.debug('Document Load and Ready');
 
+  var idSelect = document.getElementById('idSelect');
 
-  let alumnos = [{
-      "nombre": "Occonnor",
-      "genero": "F",
-      "avatar": "img/avatar1.png"
-    },
-    {
-      "nombre": "Pepa",
-      "genero": "F",
-      "avatar": "img/avatar2.png"
-    },
-    {
-      "nombre": "JoseMari",
-      "genero": "H",
-      "avatar": "img/avatar3.png"
-    },
-    {
-      "nombre": "JuanCarlos",
-      "genero": "H",
-      "avatar": "img/avatar4.png"
-    },
-    {
-      "nombre": "Monica",
-      "genero": "F",
-      "avatar": "img/avatar5.png"
-    },
-    {
-      "nombre": "Alberto",
-      "genero": "H",
-      "avatar": "img/avatar6.png"
-    },
-    {
-      "nombre": "Rafael",
-      "genero": "H",
-      "avatar": "img/avatar7.png"
-    }
-  ]
+  idSelect.addEventListener('change', seleccionarPersonas);
+  pintarListado(personas);
 
-  let listado = document.getElementById('alumnos');
-  listado.innerHTML = '';
+  
+}
 
-  for (let i = 0; i < alumnos.length; i++) {
-    const alumno = alumnos[i];
-    listado.innerHTML += `<li class="border border-dark p-1"> <img src=${alumno.avatar} class="border border-danger rounded-circle"> ${alumno.nombre}</li> `
+function seleccionarPersonas() {
+  var valor = this.value;
+  console.debug('Seleccionamos las personas con sexo: ' + valor)
+  if (valor != 'T'){
+    const peronasFiltradas = personas.filter(el => el.sexo == valor);
+    pintarListado(peronasFiltradas);
+  }else{
+    pintarListado(personas);
   }
 
 
+}
 
+function pintarListado(arrayPersonas) {
+  let listado = document.getElementById('alumnos');
+  listado.innerHTML = '';
 
-
-
+  arrayPersonas.forEach(el => listado.innerHTML += `<li class="border border-dark p-1"> <img src=${el.avatar} class="border border-danger rounded-circle"> ${el.nombre}</li> `);
+  console.debug(arrayPersonas);
 }
