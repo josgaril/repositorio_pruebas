7503883bc1bfa5377b21f398f295e01861cc2a5a
josgaril <josgaril@hotmail.com>
2020-04-25 19:12:32 +0200

    Añadido objeto profesor a la clase curso Modificadas consultas CursoDAO

--- apprest/src/com/ipartek/formacion/model/Curso.java
+++ apprest/src/com/ipartek/formacion/model/Curso.java
@@ -1,79 +1,91 @@
 package com.ipartek.formacion.model;
 
-import java.math.BigDecimal;
-
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 public class Curso {
-	
-	//Variables
-	private int id;
-	
-	@Size (min=2, max=100, message="El nombre tiene que tener entre 2 y 100 caracteres")
-	private String nombre;
-	
-	@NotEmpty (message = "La imágen del curso no puede ser nula")
-	private String imagen;
-	
-	@Positive (message="El precio del curso debe ser positivo")
-	private float precio;
-	
-	//Constructores
-	public Curso() {
-		super();
-		this.id = 0;
-		this.nombre = "";
-		this.imagen = "";
-		this.precio = 0;
-	}
-	
-	public Curso(int id, String nombre, String imagen, float precio) {
-		this();
-		this.id = id;
-		this.nombre = nombre;
-		this.imagen = imagen;
-		this.precio = precio;
-	}
+    
+    //Variables
+    private int id;
+    
+    @Size (min=2, max=100, message="El nombre tiene que tener entre 2 y 100 caracteres")
+    private String nombre;
+    
+    @NotEmpty (message = "La imágen del curso no puede ser nula")
+    private String imagen;
+    
+    @Positive (message="El precio del curso debe ser positivo")
+    private float precio;
+    
+    @NotNull 
+    @Valid //Valida la Persona
+    private Persona profesor;
+    
+    //Constructores
+    public Curso() {
+        /*
+         * super(); this.id = 0; this.nombre = ""; this.imagen = ""; this.precio = 0;
+         * this.profesor = new Persona();
+         */
+    }
+    
+    public Curso(int id, String nombre, String imagen, float precio, Persona profesor) {
+        this();
+        this.id = id;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.profesor = profesor;
+    }
 
-	//Getters y Setters
-	public int getId() {
-		return id;
-	}
+    //Getters y Setters
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public String getImagen() {
-		return imagen;
-	}
+    public String getImagen() {
+        return imagen;
+    }
 
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
-	}
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 
-	public float getPrecio() {
-		return precio;
-	}
+    public float getPrecio() {
+        return precio;
+    }
 
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
 
-	//ToString
-	@Override
-	public String toString() {
-		return "Curso [id=" + id + ", nombre=" + nombre + ", imagen=" + imagen + ", precio=" + precio + "]";
-	}
-		
+    public Persona getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Persona profesor) {
+        this.profesor = profesor;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso [id=" + id + ", nombre=" + nombre + ", imagen=" + imagen + ", precio=" + precio + ", profesor="
+                + profesor + "]";
+    }
+        
 }
--- apprest/src/com/ipartek/formacion/model/dao/CursoDAO.java
+++ apprest/src/com/ipartek/formacion/model/dao/CursoDAO.java
@@ -9,126 +9,175 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import com.ipartek.formacion.model.Curso;
+import com.ipartek.formacion.model.Persona;
 
 public class CursoDAO implements IDAO<Curso>{
-	private static final Logger LOGGER = Logger.getLogger(CursoDAO.class.getCanonicalName());
-		
-	private static CursoDAO INSTANCE = null;
-	
-	private static final String SQL_GET_ALL = "SELECT id, nombre, imagen, precio FROM curso ORDER BY id DESC LIMIT 100";
-	private static final String SQL_GET_ALL_FILTER = "SELECT id, nombre, imagen, precio FROM curso WHERE nombre LIKE ? LIMIT 100";
+    private static final Logger LOGGER = Logger.getLogger(CursoDAO.class.getCanonicalName());
+        
+    private static CursoDAO INSTANCE = null;
+    
+    private static final String SQL_GET_ALL = 
+            "   SELECT \n" + 
+            "   c.id as curso_id, \n" + 
+            "   c.nombre as curso_nombre, \n" + 
+            "   c.imagen as curso_imagen, \n" + 
+            "   c.precio as curso_precio,\n" + 
+            "   pf.id as profesor_id, \n" + 
+            "   pf.nombre as profesor_nombre, \n" + 
+            "   pf.avatar as profesor_avatar, \n" + 
+            "   pf.sexo as profesor_sexo\n" + 
+            "   FROM curso c \n" + 
+            "   JOIN persona pf ON c.profesor = pf.id \n" + 
+            "   ORDER BY c.id desc\n" + 
+            "   LIMIT 500";
 
-	private static final String SQL_GET_BY_ID = "SELECT id, nombre, imagen, precio FROM curso WHERE id=?";
+    private static final String SQL_GET_ALL_FILTER =             
+    		"   SELECT \n" + 
+            "   c.id as curso_id, \n" + 
+            "   c.nombre as curso_nombre, \n" + 
+            "   c.imagen as curso_imagen, \n" + 
+            "   c.precio as curso_precio,\n" + 
+            "   pf.id as profesor_id, \n" + 
+            "   pf.nombre as profesor_nombre, \n" + 
+            "   pf.avatar as profesor_avatar, \n" + 
+            "   pf.sexo as profesor_sexo\n" + 
+            "   FROM curso c \n" + 
+            "   JOIN persona pf ON c.profesor = pf.id \n" + 
+            "   WHERE c.nombre LIKE ? \n" + 	
+            " 	ORDER BY c.id desc\n" + 
+            "	LIMIT 100";
 
-	private CursoDAO() {
-		super();
-	}
-	
-	public synchronized static CursoDAO getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new CursoDAO();
-		}
-		return INSTANCE;
-	}
-	
-	@Override
-	public List<Curso> getAll() {
-		LOGGER.info("Obtener todos los cursos disponibles");
-		
-		ArrayList<Curso> cursos = new ArrayList<Curso>();
-		
-		try(Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL);
-				ResultSet rs = pst.executeQuery();)
-			{	
-				LOGGER.info(pst.toString());
-					
-				while (rs.next()) {
-					cursos.add(mapper(rs)); 
-				}
-			}catch (SQLException e) {
-				e.printStackTrace();
-			}
-		
-		return cursos;
-	}
-	
-	
-		public List<Curso> getAllFilter(String filtro) throws Exception{
-		LOGGER.info("Obtener todos los cursos disponibles con filtro: " + filtro);
-		
-		ArrayList<Curso> cursos = new ArrayList<Curso>();
-		
-		try(Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL_FILTER);)
-		
-		{
-			pst.setString(1, '%' + filtro + '%');
-			LOGGER.info(pst.toString());
-				
-			try (ResultSet rs = pst.executeQuery()){
+    private static final String SQL_GET_BY_ID =             
+    		"   SELECT \n" + 
+            "   c.id as curso_id, \n" + 
+            "   c.nombre as curso_nombre, \n" + 
+            "   c.imagen as curso_imagen, \n" + 
+            "   c.precio as curso_precio,\n" + 
+            "   pf.id as profesor_id, \n" + 
+            "   pf.nombre as profesor_nombre, \n" + 
+            "   pf.avatar as profesor_avatar, \n" + 
+            "   pf.sexo as profesor_sexo\n" + 
+            "   FROM curso c \n" + 
+            "   JOIN persona pf ON c.profesor = pf.id \n" + 
+            "   WHERE c.id = ?";
 
-					while (rs.next()) {
-						cursos.add(mapper(rs)); 
-					}
-				}
-			}catch (SQLException e) {
-				e.printStackTrace();
-			}
-		
-		return cursos;
-	}
+    private CursoDAO() {
+        super();
+    }
+    
+    public synchronized static CursoDAO getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new CursoDAO();
+        }
+        return INSTANCE;
+    }
+    
+    @Override
+    public List<Curso> getAll() {
+        LOGGER.info("Obtener todos los cursos disponibles");
+        
+        ArrayList<Curso> cursos = new ArrayList<Curso>();
+        
+        try(Connection con = ConnectionManager.getConnection();
+                PreparedStatement pst = con.prepareStatement(SQL_GET_ALL);
+                ResultSet rs = pst.executeQuery();)
+            {   
+                LOGGER.info(pst.toString());
+                    
+                while (rs.next()) {
+                    cursos.add(mapper(rs)); 
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        
+        return cursos;
+    }
+    
+    
+        public List<Curso> getAllFilter(String filtro) throws Exception{
+        LOGGER.info("Obtener todos los cursos disponibles con filtro: " + filtro);
+        
+        ArrayList<Curso> cursos = new ArrayList<Curso>();
+        
+        try(Connection con = ConnectionManager.getConnection();
+                PreparedStatement pst = con.prepareStatement(SQL_GET_ALL_FILTER);)
+        
+        {
+            pst.setString(1, '%' + filtro + '%');
+            LOGGER.info(pst.toString());
+                
+            try (ResultSet rs = pst.executeQuery()){
 
-	@Override
-	public Curso getById(int id) throws Exception {
-		
-		Curso curso = null;
-		try(Connection con = ConnectionManager.getConnection();
-			PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID);)
-		{	
-			pst.setInt(1, id);
-			LOGGER.info(pst.toString());
-			
-			try(ResultSet rs = pst.executeQuery()){
-				
-				if (rs.next()) {
-					curso = mapper(rs);
-				}else {
-					throw new Exception("No se ha encontrado el curso (" + id + ")" );
-				}
-				
-			}catch(SQLException e) {
-				e.printStackTrace();
-				throw new SQLException("Error al acceder a los registros de cursos disponibles");	
-			}
-		}catch (Exception e) {
-				e.printStackTrace();
-				throw new Exception("No se ha podido obtener el curso (" + id + ")" );	
-		}
-		return curso;
-	}
+                    while (rs.next()) {
+                        cursos.add(mapper(rs)); 
+                    }
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        
+        return cursos;
+    }
 
-	@Override
-	public Curso insert(Curso pojo) throws Exception, SQLException {
-		throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
-	}
+    @Override
+    public Curso getById(int id) throws Exception {
+        
+        Curso curso = null;
+        try(Connection con = ConnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID);)
+        {   
+            pst.setInt(1, id);
+            LOGGER.info(pst.toString());
+            
+            try(ResultSet rs = pst.executeQuery()){
+                
+                if (rs.next()) {
+                    curso = mapper(rs);
+                }else {
+                    throw new Exception("No se ha encontrado el curso (" + id + ")" );
+                }
+                
+            }catch(SQLException e) {
+                e.printStackTrace();
+                throw new SQLException("Error al acceder a los registros de cursos disponibles");   
+            }
+        }catch (Exception e) {
+                e.printStackTrace();
+                throw new Exception("No se ha podido obtener el curso (" + id + ")" );  
+        }
+        return curso;
+    }
 
-	@Override
-	public Curso update(Curso pojo) throws Exception, SQLException {
-		throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
-	}
+    @Override
+    public Curso insert(Curso pojo) throws Exception, SQLException {
+        throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
+    }
 
-	@Override
-	public Curso delete(int id) throws Exception, SQLException {
-		throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
-	}
+    @Override
+    public Curso update(Curso pojo) throws Exception, SQLException {
+        throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
+    }
 
-	private Curso mapper(ResultSet rs) throws SQLException{
-		Curso c = new Curso();
-		c.setId(rs.getInt("id"));
-		c.setNombre(rs.getString("nombre"));
-		c.setImagen(rs.getString("imagen"));
-		c.setPrecio(rs.getFloat("precio"));
-		return c;
-	}
+    @Override
+    public Curso delete(int id) throws Exception, SQLException {
+        throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
+    }
+
+    private Curso mapper(ResultSet rs) throws SQLException{
+        Curso c = new Curso();
+        c.setId(rs.getInt("curso_id"));
+        c.setNombre(rs.getString("curso_nombre"));
+        c.setImagen(rs.getString("curso_imagen"));
+        c.setPrecio(rs.getFloat("curso_precio"));
+        
+        Persona profesor = new Persona();
+        profesor.setId(rs.getInt("profesor_id"));
+        profesor.setNombre(rs.getString("profesor_nombre"));
+        profesor.setAvatar(rs.getString("profesor_avatar"));
+        profesor.setSexo(rs.getString("profesor_sexo"));
+
+        c.setProfesor(profesor);
+        return c;
+    }
 }
