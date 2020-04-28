3bda0b097e187d76fe5dc5fd5603a734daecc2ba
Jose Angel Garcia Illera <josgaril@hotmail.com>
2020-04-28 20:00:45 +0200

    Modificado script BBDD y agregado diagrama

--- apprest/script-db.sql
+++ apprest/script-db.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `alumnos` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alumnos`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost    Database: alumnos
+-- Host: 127.0.0.1    Database: alumnos
 -- ------------------------------------------------------
--- Server version	5.7.29-0ubuntu0.18.04.1
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,22 +18,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `alumnos`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `alumnos` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `alumnos`;
-
---
 -- Table structure for table `curso`
 --
 
 DROP TABLE IF EXISTS `curso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `curso` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `imagen` varchar(250) NOT NULL DEFAULT 'curso.png',
   `precio` decimal(6,2) NOT NULL,
@@ -56,10 +50,10 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `cursos_contratados`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cursos_contratados` (
-  `id_persona` int(11) NOT NULL,
-  `id_curso` int(11) NOT NULL,
+  `id_persona` int NOT NULL,
+  `id_curso` int NOT NULL,
   PRIMARY KEY (`id_persona`,`id_curso`),
   KEY `cursos_contratados-curso_FK` (`id_curso`),
   CONSTRAINT `cursos_contratados-curso_FK` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`),
@@ -83,15 +77,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `noticia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `noticia` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `titulo` varchar(250) NOT NULL,
   `fecha` datetime NOT NULL,
-  `contenido` text NOT NULL,
+  `contenido` longtext NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `noticia_UN` (`titulo`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +94,7 @@ CREATE TABLE `noticia` (
 
 LOCK TABLES `noticia` WRITE;
 /*!40000 ALTER TABLE `noticia` DISABLE KEYS */;
-INSERT INTO `noticia` VALUES (1,'Se reanudan las clases','2020-04-23 14:00:00','Las clases se reanudarán el prómimo 11 de mayo.'),(2,'Vuelve el futbol','2020-04-24 00:00:00','Se prevee que los equipos puedan volver a los entrenamientos el próximo 1 de Mayo'),(3,'Los Rolling en España','2020-04-24 00:05:00','Se dice se comenta que los Rolling Stones podrían reunirse en España cuando acabe el confinamiento para decidir qsi harán su última función'),(4,'Bilbao sin contagiados','2020-04-24 00:08:00','El gobierno indica que Bilbao es la primera ciudad en la que no hay contagiados por el virus');
+INSERT INTO `noticia` VALUES (1,'Se reanudan las clases','2020-04-23 14:00:00','Las clases se podrás reanudar a finales de mayo si todo va bien'),(2,'Vuelve el futbol','2020-04-24 00:00:00','Se prevee que los equipos puedan volver a los entrenamientos el próximo 1 de Mayo'),(3,'Los Rolling en España','2020-04-24 00:05:00','Se dice se comenta que los Rolling Stones podrían reunirse en España cuando acabe el confinamiento para decidir si harán su última función'),(4,'Bilbao sin contagiados','2020-04-24 00:08:00','El gobierno indica que Bilbao es la primera ciudad en la que no hay contagiados por el virus</pv>'),(6,'Oficial: el Gobierno de Francia confirma la suspensión definitiva de la la Ligue 1','2020-04-28 17:30:00','<div class=\"row content cols-30-70\"><figure class=\"multimedia-item image\"><img src=\"https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/04/28/15880811359102.jpg\" class=\"full-image\" alt=\"Futbol Francia Suspendida Ligue 1 Liga Francesa\"><figcaption class=\"caption\"><i class=\"icon-caption-photo\"></i>Panorámica del Parque de los Príncipes. <br></figcaption></figure><div class=\"secondary-column\"><aside class=\"aside-tools byline-tools\"><div class=\"aside-block content-ad--sticky\"> <a class=\"language-tools\" href=\"https://www.marca.com/en/football/international-football/2020/04/28/5ea84135ca4741781f8b458e.html\" lang=\"en\"><i class=\"icon-english\"></i> In English </a><a href=\"#\" class=\"comments-btn js-comentar-5ea8329a22601d59028b45b0\"><span class=\"left\"><i class=\"icon-comments_24x20_a\"></i><strong>343</strong></span><span class=\"hidden-content\"> comentarios</span><span class=\"right\"><strong>Comentar</strong><i class=\"icon-forward_8x16_a\"></i></span></a><div class=\"content-ad\"><div class=\"ad-item-300 ue-c-ad--label\"><div id=\"div-gpt-ad-futbol_liga-francesa_n_r_5ea8329a22601d59028b45b0\" class=\"ad-item\"><script type=\"text/javascript\">googletag.cmd.push(function(){googletag.display(\'div-gpt-ad-futbol_liga-francesa_n_r_5ea8329a22601d59028b45b0\');});</script></div></div></div></div></aside><aside class=\"publicidad mod-multipurpose end-fixed\"><h3 class=\"list-header\"><span>TE PUEDE INTERESAR</span></h3><div id=\"div-gpt-ad-futbol_liga-francesa_n_nata_5ea8329a22601d59028b45b0\" class=\"ad-item\"><script type=\"text/javascript\">googletag.cmd.push(function(){googletag.display(\'div-gpt-ad-futbol_liga-francesa_n_nata_5ea8329a22601d59028b45b0\');});</script></div><div class=\"dotted-divider\"></div><div id=\"div-gpt-ad-futbol_liga-francesa_n_natb_5ea8329a22601d59028b45b0\" class=\"ad-item\"><script type=\"text/javascript\">googletag.cmd.push(function(){googletag.display(\'div-gpt-ad-futbol_liga-francesa_n_natb_5ea8329a22601d59028b45b0\');});</script></div></aside></div><aside class=\"content-ad\"><div class=\"ad-item-660 ue-c-ad--label\"><div id=\"div-gpt-ad-futbol_liga-francesa_n_660x50_5ea8329a22601d59028b45b0\" class=\"ad-item\"><script type=\"text/javascript\">googletag.cmd.push(function(){googletag.display(\'div-gpt-ad-futbol_liga-francesa_n_660x50_5ea8329a22601d59028b45b0\');});</script></div></div></aside><p><strong>La temporada de la Ligue 1 está terminada</strong>. El primer ministro Edouard Philippe anunció en su discurso ante la Asamblea Nacional de este martes por la tarde que no habrá fútbol en Francia hasta septiembre. \"Los eventos que reúnen a más de 5000 participantes (...) no pueden realizarse antes de septiembre\", aseguró Philippe.</p><p>\"Será posible, en días soleados, practicar una actividad deportiva individual al aire libre, respetando las reglas de distanciamiento social.<strong> No será posible ni practicar deporte en lugares cubiertos, ni los deportes de equipo o de contacto\"</strong>, afirmó el primer ministro \'cancelando\' de facto las Ligas de fútbol y de otros deportes colectivos como el rugby.</p><aside class=\"summary-item\"><aside class=\"citas\"><blockquote class=\"summary-cite\">No será posible ni practicar deporte en lugares cubiertos, ni los deportes de equipo o de contacto hasta septiembre\"</blockquote><p class=\"cite-author\">Edouard Philippe, primer ministro de Francia</p></aside></aside><p><strong>El Gobierno anunció el plan de desconfinamiento de Francia (se iniciará a partir del 11 de mayo) y en él el fútbol no podrá reanudarse hasta septiembre al no cumplirse las condiciones sanitarias mínimas para su retorno.</strong> Un plan que choca con la propuesta de la Ligue 1, que esperaba retomar los entrenamientos en mayo y poder jugar a puerta cerrada más adelante, completando sus competiciones en junio y julio. </p><p>Ahora, la Liga y la Federación francesa deberán reunirse para ver qué hacen con los ascensos, descensos, el campeón... y cómo afecta esto a la próxima campaña. <strong>Esta decisión va en la línea de la adoptada por el gobierno holandés</strong>, que prohibió cualquier evento deportivo hasta septiembre, lo que provocó la cancelación de la temporada de la Eredivisie dejando el título desierto, suprimiendo los ascensos y los descensos y confirmando los equipos clasificados para la Champions y la Europa League por su clasificación.</p><script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src=\'https://embed.ex.co/sdk.js\';fjs.parentNode.insertBefore(js,fjs);}(document,\'script\',\'playbuzz-sdk\'));</script><div class=\"playbuzz\" data-id=\"d84180db-e82f-4d42-829c-bc7d21c53523\" data-show-share=\"false\" data-show-info=\"false\" data-rendered=\"true\"><iframe allowfullscreen=\"true\" scrolling=\"no\" data-id=\"d84180db-e82f-4d42-829c-bc7d21c53523\" data-show-share=\"false\" data-show-info=\"false\" data-sdk-load-id=\"3625cd55-caa8-4c9d-9833-9657adb6b3dd\" style=\"width: 1px; min-width: 100%; height: 425px; border: 0px;\"></iframe></div><aside class=\"related-tags\"><h3 class=\"list-header\"><span>Temas relacionados</span></h3><ul class=\"item-tags\"><li><a href=\"https://www.marca.com/futbol/liga-francesa.html\" rel=\"tag\">Ligue 1</a></li><li><a href=\"https://www.marca.com/polideportivo/coronavirus.html\" rel=\"tag\">Coronavirus</a></li></ul></aside></div>');
 /*!40000 ALTER TABLE `noticia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,15 +104,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `persona`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `persona` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `avatar` varchar(250) NOT NULL DEFAULT 'avatar7.png',
   `sexo` varchar(1) NOT NULL DEFAULT 'h',
   PRIMARY KEY (`id`),
   UNIQUE KEY `persona_UN` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,6 +124,14 @@ LOCK TABLES `persona` WRITE;
 INSERT INTO `persona` VALUES (2,'Lorena','avatar2.png','m'),(17,'Alba','avatar5.png','m'),(24,'Manolo','avatar6.png','h'),(35,'Pedro','avatar7.png','h'),(39,'Marisa','avatar5.png','m'),(40,'Rebeca','avatar2.png','m');
 /*!40000 ALTER TABLE `persona` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'alumnos'
+--
+
+--
+-- Dumping routines for database 'alumnos'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -140,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-24  9:33:53
+-- Dump completed on 2020-04-28 19:59:42
