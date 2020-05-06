3abb078b418426151d1d1d91dc2b7c80962bccfa
Jose Angel Garcia Illera <josgaril@hotmail.com>
2020-05-06 16:47:11 +0200

    Modificado IPERSONADAO

--- apprest/src/com/ipartek/formacion/model/dao/IPERSONADAO.java
+++ apprest/src/com/ipartek/formacion/model/dao/IPERSONADAO.java
@@ -1,38 +1,30 @@
 package com.ipartek.formacion.model.dao;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import com.ipartek.formacion.model.Persona;
 
-public interface IPERSONADAO extends IDAO<Persona>{
+public interface IPERSONADAO extends IDAO<Persona> {
 
 	/**
-	 * Se obtienen las personas, filtrando por su Rol para saber si son alumnos o profesores
-	 * @param rol
-	 * @return
-	 * @throws Exception En caso de que Rol no exista o Rol == null
-	 */
-	List<Persona> getAllByRol( String rol ) throws Exception;
-
-	/**
-	 * Se contrata un curso para el alumno. Se pasa como parámetro el id del alumno y el id del curso a contratar
+	 * Se contrata un curso para el alumno. Se pasa como parámetro el id del alumno
+	 * y el id del curso a contratar
+	 * 
 	 * @param idPersona
 	 * @param idCurso
 	 * @return
-	 * @throws Exception si no encuentra el el alumno o el curso
+	 * @throws Exception    si no encuentra el el alumno o el curso
 	 * @throws SQLException Si ya tiene contratado ese curso
 	 */
 	boolean contratarCurso(int idPersona, int idCurso) throws Exception, SQLException;
- 
+
 	/**
 	 * 
 	 * @param idPersona
 	 * @param idCurso
 	 * @return
-	 * @throws Exception Si no encuentra el curso contratado a eliminar 
+	 * @throws Exception Si no encuentra el curso contratado a eliminar
 	 */
 	boolean eliminarCursoContratado(int idPersona, int idCurso) throws Exception;
-	
-	
+
 }
